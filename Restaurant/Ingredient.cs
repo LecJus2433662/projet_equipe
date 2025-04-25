beacae584d420ab4cc11a2690085a5b4de04ed83
TurAle2433991 <aturcotte2007@icloud.com>
2025-04-25 10:33:09 -0400

    comit

---
+++ Restaurant/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Ingredient
+    {
+        public string Nom { get; set; }
+        public double PrixAchat { get; set; }
+    }
+}
