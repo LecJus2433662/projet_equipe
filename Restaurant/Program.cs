4f32aa4bb57ca187d966e638c8500cbd896478eb
TurAle2433991 <aturcotte2007@icloud.com>
2025-04-25 10:17:15 -0400

    commit menu

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -6,14 +6,12 @@ namespace Restaurant
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
 
             Menu platMatin = new Menu("Menu du matin", 5);
 
             Console.WriteLine("Appuyer sur ENTER pour afficher votre menu");
             Console.WriteLine("*********************************** MATIN  *************************************************");
             Console.WriteLine($"{platMatin}\n");
-=======
             Random rnd = new Random();
             int nbRandom = rnd.Next(6, 11);
             for (int i = 0; i < nbRandom; i++)
@@ -22,8 +20,7 @@ namespace Restaurant
                 Console.WriteLine(visiteur);
 
             }
-            
->>>>>>> main
+
         }
     }
 }
