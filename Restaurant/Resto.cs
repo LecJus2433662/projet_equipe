3bfc26be7ec9b0986ccd892819b41d3c5f59fc2c
LecJus2433662 <justinleclerc11@gmail.com>
2025-04-28 09:31:21 -0400

    commit

---
+++ Restaurant/Resto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Resto
+    {
+        int total {  get; set; }
+       
+        
+        
+        public Resto()
+        {
+            total = 10000;
+            Client client = new Client();
+            Menu menu = new Menu();
+            Plat plat = new Plat("",34);
+            menu.AjouterPlat(plat);
+            
+        }
+
+
+        
+
+
+    }
+}
