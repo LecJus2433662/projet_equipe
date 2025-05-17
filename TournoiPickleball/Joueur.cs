217ef4a18c5a94feea1d4e3eb0edba3ec0613b1d
LecJus2433662 <justinleclerc11@gmail.com>
2025-05-17 13:01:09 -0400

    comimt

--- TournoiPickleball/Joueur.cs
+++ TournoiPickleball/Joueur.cs
@@ -30,22 +30,34 @@ namespace TournoiPickleball
         public Joueur()
         {
             int rando = Random.Next(1,4);
+            string[] noms = new string[] { "Labrador", "Vincent", "Francis", "Sophie", "Julien", "Marie", "Alexandre", "Émilie", "Thomas", "Chloé", "Maxime", "Léa", "Gabriel", "Camille", "Lucas", "Amélie", "Hugo", "Sarah", "Mathieu", "Clara" };
+           
+            nom = noms[Random.Next(noms.Length)];
 
             switch (rando)
             {
                 case 1:
                     NiveauExperience = NiveauExperience.debutant;
                     force = Random.Next(1,4);
+                    dexterite = Random.Next(1,4);
 
                     break;
                     case 2:
                     NiveauExperience = NiveauExperience.intermediaire;
+                    force = Random.Next(2, 8);
+                    dexterite = Random.Next(2, 8);
                     break;
                     case 3:
                     NiveauExperience = NiveauExperience.professionnel;
+                    force = Random.Next(3, 11);
+                    dexterite = Random.Next(3, 11);
                     break;
             }
-        }
 
+        }
+        public override string ToString()
+        {
+            return $"nom du joueur : {nom} - force : {force} - dextérité : {dexterite} - niveau d'expérience du joueur : {NiveauExperience}";
+        }
     }
 }
